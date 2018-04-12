8e64fae822d5d4b5fc523ce6e5f384bcf070f694
Toaignika <eugene.my88@gmail.com>
2018-04-12 09:14:18 -0700

    Commit suicide

--- app/src/main/java/com/example/toa/finalexam/MainActivity.java
+++ app/src/main/java/com/example/toa/finalexam/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.toa.finalexam;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements
+        TextToSpeech.OnInitListener{
+
+    private TextToSpeech tts;
+    private Button btnSpeak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tts = new TextToSpeech(this, this);
+        btnSpeak = (Button) findViewById(R.id.btn_speak);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakOut();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.JAPAN);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            } else {
+               // speakOut();
+            }
+
+        } else {
+        }
+
+    }
+
+    private void speakOut() {
+        String text = "three zero zero";
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        Toast.makeText(this, "hey", Toast.LENGTH_SHORT).show();
     }
 }
